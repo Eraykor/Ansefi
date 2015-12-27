d2207fee975595e9efe02d25f1d4ad0d4d919e8f
lijinhao <contact.li.jinhao@gmail.com>
2015-12-27 01:21:42 +0100

    finalisation du design général du site

--- js/playliste.js
+++ js/playliste.js
@@ -1,135 +1,203 @@
-/* 
- * HTML5 Audio player with playlist
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2012, Script Tutorials
- * http://www.script-tutorials.com/
- */
-jQuery(document).ready(function() {
+"use strict";
 
-    // inner variables
-    var song;
-    var tracker = $('.tracker');
-    var volume = $('.volume');
+var tracks = [
+    "data/03_Silly_Go_Round.mp3",
+    "data/04 - angel gate.mp3",
+    "data/angel gate.mp3"
+];
 
-    function initAudio(elem) {
-        var url = elem.attr('audiourl');
-        var title = elem.text();
-        var cover = elem.attr('cover');
-        var artist = elem.attr('artist');
+var currentTrackIndex = 0;
+var audioPlayer;
+var audioTrack;
+var audioTitle;
+var playButton;
+var nextButton;
+var prevButton;
+var muteButton;
+var volumeSlider;
+var progressSlider;
 
-        $('.player .title').text(title);
-        $('.player .artist').text(artist);
-        $('.player .cover').css('background-image','url(data/' + cover+')');;
+function setText(elem, text) {
+    elem.innerHTML = text;
+}
 
-        song = new Audio('data/' + url);
+function setAttributes(elem, attrs) {
+    for (var k in attrs) {
+        elem.setAttribute(k, attrs[k]);
+    }
+}
 
-        // timeupdate event listener
-        song.addEventListener('timeupdate',function (){
-            var curtime = parseInt(song.currentTime, 10);
-            tracker.slider('value', curtime);
+function initPlayer() {
+    audioPlayer = document.getElementById("audioplayer");
+    audioTrack = document.getElementById("audiotrack");
+
+    // create play button
+    playButton = document.createElement("button");
+    playButton.type = "button";
+    audioPlayer.appendChild(playButton);
+    audioTrack.removeAttribute("controls");
+    audioTrack.addEventListener("play", function () {
+        setText(playButton, "Pause");
+    });
+    audioTrack.addEventListener("playing", function () {
+        setText(playButton, "Pause");
+    });
+    audioTrack.addEventListener("pause", function () {
+        setText(playButton, "Play");
+    });
+    setText(playButton, "Play");
+
+    // create previous button
+    prevButton = document.createElement("button");
+    prevButton.type = "button";
+    audioPlayer.appendChild(prevButton);
+    setText(prevButton, "Previous");
+
+    // create next button
+    nextButton = document.createElement("button");
+    nextButton.type = "button";
+    audioPlayer.appendChild(nextButton);
+    setText(nextButton, "Next");
+
+    // create mute button
+    muteButton = document.createElement("button");
+    muteButton.type = "button";
+    audioPlayer.appendChild(muteButton);
+    setText(muteButton, "Mute")
+
+    // create volume slider
+    volumeSlider = document.createElement("input");
+    volumeSlider.type = "range";
+    audioPlayer.appendChild(volumeSlider);
+    volumeSlider.style.width = "100px";
+    volumeSlider.style.display = "inline-block";
+    setAttributes(volumeSlider, {
+        "type": "range",
+        "min": "0",
+        "max": "1",
+        "step": "any",
+        "value": "1"
+    });
+
+    // create progression slider
+    progressSlider = document.createElement("input");
+    progressSlider.type = "range";
+    audioPlayer.appendChild(progressSlider);
+    progressSlider.style.width = "auto";
+    progressSlider.style.display = "inline-block";
+
+    // create audio title
+    audioTitle = document.createElement("div");
+    audioTitle.style.display = "inline-block";
+    audioPlayer.appendChild(audioTitle);
+
+    playButton.addEventListener("click", playPause);
+    nextButton.addEventListener("click", nextTrack);
+    prevButton.addEventListener("click", prevTrack);
+    muteButton.addEventListener("click", muteUnmute);
+    volumeSlider.addEventListener("input", changeVolume);
+    audioTrack.addEventListener("volumechange", updateMuteButton);
+
+    progressSlider.addEventListener("input", updateTrackProgress);
+    audioTrack.addEventListener("timeupdate", seekTrackProgress);
+
+    audioTrack.addEventListener("loadstart", function () {
+        setText(audioTitle, "downloading...");
+    });
+
+    audioTrack.onerror = function () {
+        setText(audioTitle, "error loading track");
+    };
+    audioTrack.addEventListener("loadeddata", function () {
+        audioTrack.play();
+        setText(audioTitle, audioTrack.currentSrc);
+    });
+
+    audioTrack.addEventListener("loadedmetadata", function () {
+        setAttributes(progressSlider, {
+            "type": "range",
+            "min": "0",
+            "max": audioTrack.duration,
+            "step": "any",
+            "value": "0"
         });
+    });
 
-        $('.playlist li').removeClass('active');
-        elem.addClass('active');
+    audioTrack.addEventListener("durationchange", function () {
+        setAttributes(progressSlider, {
+            "type": "range",
+            "min": "0",
+            "max": audioTrack.duration,
+            "step": "any",
+            "value": "0"
+        });
+    });
+
+    audioTrack.addEventListener("ended", function () {
+        nextTrack();
+    });
+}
+
+function loadCurrentTrack() {
+    loadTrack(tracks[currentTrackIndex]);
+}
+
+function loadTrack(track) {
+    audioTrack.src = track;
+    audioTrack.load();
+
+    setText(audioTitle, "loading...");
+}
+
+function playPause() {
+    if (audioTrack.paused) {
+        audioTrack.play();
+    } else {
+        audioTrack.pause();
     }
-    function playAudio() {
-        song.play();
+}
 
-        tracker.slider("option", "max", song.duration);
+function nextTrack() {
+    currentTrackIndex++;
+    if (currentTrackIndex >= tracks.length)
+        currentTrackIndex = 0;
+    loadCurrentTrack();
+}
 
-        $('.play').addClass('hidden');
-        $('.pause').addClass('visible');
+function prevTrack() {
+    currentTrackIndex--;
+    if (currentTrackIndex < 0)
+        currentTrackIndex = tracks.length - 1;
+    loadCurrentTrack();
+}
+
+function muteUnmute() {
+    if (audioTrack.volume == 0) {
+        audioTrack.volume = 1;
+    } else {
+        audioTrack.volume = 0;
     }
-    function stopAudio() {
-        song.pause();
 
-        $('.play').removeClass('hidden');
-        $('.pause').removeClass('visible');
+    updateMuteButton();
+}
+
+function changeVolume() {
+    audioTrack.volume = volumeSlider.value;
+}
+
+function updateMuteButton() {
+    volumeSlider.value = audioTrack.volume;
+    if (audioTrack.volume != 0) {
+        setText(muteButton, "Mute");
+    } else {
+        setText(muteButton, "Unmute");
     }
+}
 
-    // play click
-    $('.play').click(function (e) {
-        e.preventDefault();
+function updateTrackProgress() {
+    audioTrack.currentTime = progressSlider.value;
+}
 
-        playAudio();
-    });
-
-    // pause click
-    $('.pause').click(function (e) {
-        e.preventDefault();
-
-        stopAudio();
-    });
-
-    // forward click
-    $('.fwd').click(function (e) {
-        e.preventDefault();
-
-        stopAudio();
-
-        var next = $('.playlist li.active').next();
-        if (next.length == 0) {
-            next = $('.playlist li:first-child');
-        }
-        initAudio(next);
-    });
-
-    // rewind click
-    $('.rew').click(function (e) {
-        e.preventDefault();
-
-        stopAudio();
-
-        var prev = $('.playlist li.active').prev();
-        if (prev.length == 0) {
-            prev = $('.playlist li:last-child');
-        }
-        initAudio(prev);
-    });
-
-    // show playlist
-    $('.pl').click(function (e) {
-        e.preventDefault();
-
-        $('.playlist').fadeIn(300);
-    });
-
-    // playlist elements - click
-    $('.playlist li').click(function () {
-        stopAudio();
-        initAudio($(this));
-    });
-
-    // initialization - first element in playlist
-    initAudio($('.playlist li:first-child'));
-
-    // set volume
-    song.volume = 0.8;
-
-    // initialize the volume slider
-    volume.slider({
-        range: 'min',
-        min: 1,
-        max: 100,
-        value: 80,
-        start: function(event, ui) {},
-        slide: function(event, ui) {
-            song.volume = ui.value / 100;
-        },
-        stop: function(event,ui) {},
-    });
-
-    // empty tracker slider
-    tracker.slider({
-        range: 'min',
-        min: 0, max: 10,
-        start: function(event,ui) {},
-        slide: function(event, ui) {
-            song.currentTime = ui.value;
-        },
-        stop: function(event,ui) {}
-    });
-});
+function seekTrackProgress() {
+    progressSlider.value = audioTrack.currentTime;
+}
